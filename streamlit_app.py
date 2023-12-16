b1da88c6bdd1c87c9d2313fa1d5b907ce70e2699
Barnaby Gray <barnaby@pickle.me.uk>
2023-12-16 06:52:52 +0000

    Move code in module

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,214 +1,20 @@
-from datetime import datetime
-from hashlib import sha256
 import hashlib
 import numpy as np
 import pendulum
 import streamlit as st
 
-from api import (
+from savingsessions import calculation, db
+from savingsessions.api import (
     API,
     AuthenticationError,
     ElectricityMeterPoint,
-    SavingSession,
 )
-import db
 
 
 @st.cache_data(ttl=None)  # never expire
 def get_product(code: str):
     api = API()  # unauthenticated
     return api.energy_product(code)
-
-
-def weekday(day):
-    """True if day is a weekday"""
-    return pendulum.MONDAY <= day.day_of_week <= pendulum.FRIDAY
-
-
-def phh(hh: int):
-    return pendulum.duration(minutes=hh * 30)
-
-
-class Readings:
-    """Cached table of readings"""
-
-    def __init__(self, meter_point: ElectricityMeterPoint):
-        self.meter_point = meter_point
-        self.requested = set()
-        self.hh = {}
-
-    def get_readings(self, api: API, ts: datetime, hh: int, debug):
-        half_hours = list(pendulum.period(ts, ts + phh(hh - 1)).range("minutes", 30))
-        if not self.requested.issuperset(half_hours):
-            start_at = ts - phh(100 - hh)
-            debug(f"Fetching {self.meter_point.mpan} readings from {start_at}")
-
-            # Request readings and cache the lot
-            readings = api.half_hourly_readings(
-                mpan=self.meter_point.mpan,
-                meter=self.meter_point.meters[0].id,
-                start_at=start_at,
-                first=100,
-                before=None,
-            )
-            if readings:
-                debug(
-                    f"Received {len(readings)} readings from {readings[0].startAt} to {readings[-1].endAt}"
-                )
-                self.requested.update(
-                    pendulum.period(start_at, readings[-1].startAt).range("minutes", 30)
-                )
-            else:
-                debug("Received no readings")
-                self.requested.update(
-                    pendulum.period(start_at, start_at + phh(99)).range("minutes", 30)
-                )
-
-            for reading in readings:
-                self.hh[reading.startAt] = reading.value
-
-        try:
-            values = [self.hh[t] for t in half_hours]
-            return np.array(values)
-        except KeyError:
-            raise ValueError("missing readings")
-
-
-class Calculation:
-    def __init__(self, ss: SavingSession) -> None:
-        self.ss = ss
-        self.session_import = None
-        self.session_export = None
-        self.baseline_days = []
-        self.baseline_import = None
-        self.baseline_export = None
-        self.baseline = None
-        self.kwh = None
-        self.points = None
-
-    def calculate(
-        self,
-        api: API,
-        sessions: list[SavingSession],
-        import_readings: Readings,
-        export_readings: Readings | None,
-        tick,
-        debug,
-    ):
-        # Baseline from meter readings from the same time as the Session over the past 10 weekdays (excluding any days with a Saving Session),
-        # past 4 weekend days if Saving Session is on a weekend.
-        days_required = 10 if weekday(self.ss.startAt) else 4
-        previous_session_days = {ss.startAt.date() for ss in sessions}
-        previous = pendulum.period(
-            self.ss.startAt.subtract(days=1), self.ss.startAt.subtract(days=61)
-        )
-
-        try:
-            self.session_import = import_readings.get_readings(
-                api, self.ss.startAt, self.ss.hh, debug
-            )
-            debug(f"session import: {self.session_import}")
-        except ValueError:
-            # incomplete, but useful to still calculate baseline
-            debug("session incomplete")
-        next(tick)
-
-        if export_readings:
-            try:
-                self.session_export = export_readings.get_readings(
-                    api, self.ss.startAt, self.ss.hh, debug
-                )
-                debug(f"session export: {self.session_export}")
-            except ValueError:
-                debug("missing export readings")
-        next(tick)
-
-        days = 0
-        baseline_import = []
-        baseline_export = []
-        for dt in previous.range("days"):
-            if weekday(dt) != weekday(self.ss.startAt):
-                continue
-            if dt.date() in previous_session_days:
-                continue
-
-            try:
-                import_values = import_readings.get_readings(api, dt, self.ss.hh, debug)
-                baseline_import.append(import_values)
-                debug(f"baseline day #{days}: {dt} import: {import_values}")
-            except ValueError:
-                debug(f"skipped day: {dt} missing readings")
-                continue
-            next(tick)
-
-            if export_readings:
-                try:
-                    export_values = export_readings.get_readings(
-                        api, dt, self.ss.hh, debug
-                    )
-                    baseline_export.append(export_values)
-                    debug(f"baseline day #{days}: {dt} export: {export_values}")
-                except ValueError:
-                    debug(f"baseline day: {dt} missing export readings")
-            next(tick)
-
-            self.baseline_days.append(dt)
-            days += 1
-            if days == days_required:
-                break
-
-        if baseline_import:
-            self.baseline_import = np.asarray(baseline_import)
-            self.baseline = self.baseline_import.mean(axis=0)
-            if baseline_export:
-                self.baseline_export = np.asarray(baseline_export)
-                self.baseline = self.baseline - self.baseline_export.mean(axis=0)
-
-            if self.session_import is not None:
-                session = self.session_import
-                if self.session_export is not None:
-                    session = session - self.session_export
-                # saving is calculated per settlement period (half hour), and only positive savings considered
-                self.kwh = (self.baseline - session).clip(min=0)
-                self.points = (
-                    np.round(self.kwh * self.ss.rewardPerKwhInOctoPoints / 8).astype(
-                        int
-                    )
-                    * 8
-                )
-
-    def row(self):
-        ret = {
-            "session": self.ss.startAt,
-        }
-        if self.session_import is not None:
-            ret["import"] = self.session_import.sum()
-        if self.session_export is not None:
-            ret["export"] = self.session_export.sum()
-        if self.baseline is not None:
-            ret["baseline"] = self.baseline.sum()
-        if self.kwh is not None:
-            ret["saved"] = self.kwh.sum()
-            reward = int(self.points.sum())
-            ret["reward"] = reward
-            ret["earnings"] = reward / 800
-        return ret
-
-    def dbrow(self, id_lookup: dict):
-        ret = {
-            "saving_session_id": id_lookup[self.ss.code],
-        }
-        if self.session_import is not None:
-            ret["session_import"] = self.session_import.sum()
-        if self.session_export is not None:
-            ret["session_export"] = self.session_export.sum()
-        if self.baseline_import is not None:
-            ret["baseline_import"] = self.baseline_import.mean(axis=0).sum()
-        if self.baseline_export is not None:
-            ret["baseline_export"] = self.baseline_export.mean(axis=0).sum()
-        if self.points is not None:
-            ret["points"] = int(self.points.sum())
-        return ret
 
 
 def error(msg: str):
@@ -355,9 +161,9 @@ def results(api_key):
         error("No import meter found.")
         raise Exception("unreachable code")
 
-    import_readings = Readings(mpans[import_mpan])
+    import_readings = calculation.Readings(mpans[import_mpan])
     if export_mpan:
-        export_readings = Readings(mpans[export_mpan])
+        export_readings = calculation.Readings(mpans[export_mpan])
     else:
         st.info("Import meter only", icon="ℹ️")
         export_readings = None
@@ -384,7 +190,7 @@ def results(api_key):
             start + ticks_per_session,
         )
         debug(f"session: {ss}")
-        calc = Calculation(ss)
+        calc = calculation.Calculation(ss)
         calc.calculate(api, sessions, import_readings, export_readings, ticks, debug)
         calcs.append(calc)
         rows.append(calc.row())
@@ -445,9 +251,9 @@ def results(api_key):
         with st.expander(f"Session {ss.startAt:%b %d %Y} breakdown"):
             timestamps = [
                 ts.strftime("%H:%M")
-                for ts in pendulum.period(ss.startAt, ss.endAt - phh(1)).range(
-                    "minutes", 30
-                )
+                for ts in pendulum.period(
+                    ss.startAt, ss.endAt - pendulum.duration(minutes=30)
+                ).range("minutes", 30)
             ]
             days = [f"{day:%b %d}" for day in calc.baseline_days]
 
